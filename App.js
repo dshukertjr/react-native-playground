2c8b882dd522d28c09a9c6bf2ee02f36982127b7
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2021-11-11 22:21:00 +0900

    feat: Added google signin

--- App.js
+++ App.js
@@ -18,6 +18,7 @@ import {
   View,
   Button,
 } from 'react-native';
+import { GoogleSignin, statusCodes } from '@react-native-google-signin/google-signin';
 
 import {
   Colors,
@@ -62,6 +63,22 @@ const App: () => Node = () => {
 
   const onPressed = async () => {
     console.log('pressed');
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      console.log(userInfo);
+      const idToken = userInfo.idToken;
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (e.g. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+      } else {
+        // some other error happened
+      }
+    }
   };
 
   return (
